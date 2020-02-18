32a8b7706463bccf25dfb7d71b2086638689b1ea
asucarlos <kuwahara.mee@gmail.com>
2020-02-17 22:31:12 -0500

    complete tutorial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './actions'
+import User from './components/users'
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
   return (
     <div className="App">
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5)) }>+</button>
+      <button onClick={() => dispatch(decrement()) }>-</button>
+      {isLogged ? <h3>Valuable Information that I shouldn't see</h3> : ""}
+      <User />
+      <User />
+      <User />
     </div>
   );
 }
